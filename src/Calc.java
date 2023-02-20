fad24d3531b7ca4742c87cca1951838ebd14fd40
Fehér Mátyás <waldorfosmatyi@gmail.com>
2023-02-20 13:59:04 +0100

    Számítás kész

---
+++ src/Calc.java
@@ -0,0 +1,14 @@
+/*
+* File: Calc.java
+* Author: Fehér Mátyás
+* Copyright: 2023, Fehér Mátyás
+* Group: Szoft I 1 N
+* Date: 2023.02.20
+* Github: https://github.com/MatiasX/
+* Licenc: GNU GPL
+*/
+public class Calc {
+    public double calculateTerulet(double a, double b) {
+        return a * b * 2;
+    }
+}
